358fbe7cea13375bee2e497937be193841e1c327
Daniel Baetz <baetz.daniel@gmail.com>
2019-06-20 15:11:20 +0200

    v1.1.0.1 - activator overloads

--- Exomia.Framework/Activator.cs
+++ Exomia.Framework/Activator.cs
@@ -23,40 +23,43 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
 namespace Exomia.Framework
 {
     /// <summary>
-    ///     A delegate that can be used to repeatedly create instances of type T using a specific constructor.
-    ///     You cannot use the same delegate for different constructors.
-    ///     You should create a delegate using GetCreator for each constructor that you need to call.
+    ///     A delegate that can be used to repeatedly create instances of type T using a specific
+    ///     constructor. You cannot use the same delegate for different constructors. You should
+    ///     create a delegate using GetCreator for each constructor that you need to call.
     /// </summary>
-    /// <typeparam name="T">The type of object that this delegate will create.</typeparam>
-    /// <param name="parameters">The parameters to create the object with.</param>
+    /// <typeparam name="T"> The type of object that this delegate will create. </typeparam>
+    /// <param name="parameters"> The parameters to create the object with. </param>
+    /// <returns>
+    ///     A T.
+    /// </returns>
     public delegate T Creator<out T>(params object[] parameters);
 
     /// <summary>
-    ///     Activator class
+    ///     Activator class.
     /// </summary>
     public static class Activator
     {
         /// <summary>
-        ///     Get a compiled lambda expression that can then be used
-        ///     to repeatedly create instances of that type using the constructor that
-        ///     the compiled lambda expression was created from.
+        ///     Get a compiled lambda expression that can then be used to repeatedly create instances of
+        ///     that type using the constructor that the compiled lambda expression was created from.
         /// </summary>
-        /// <typeparam name="TDelegate">The type of object that the compiled lambda expression will create.</typeparam>
-        /// <returns>A compiled lambda expression in the form of a delegate.</returns>
-        /// <exception cref="NotSupportedException"></exception>
-        /// <exception cref="NullReferenceException"></exception>
-        /// <exception cref="InvalidCastException"></exception>
-        public static TDelegate GetCreator<TDelegate>()
-            where TDelegate : Delegate
+        /// <param name="dType"> The type. </param>
+        /// <returns>
+        ///     A compiled lambda expression in the form of a delegate.
+        /// </returns>
+        /// <exception cref="NullReferenceException"> Thrown when a value was unexpectedly null. </exception>
+        /// <exception cref="NotSupportedException">  Thrown when the requested operation is not supported. </exception>
+        public static Delegate GetCreator(Type dType)
         {
-            Type       dType = typeof(TDelegate);
-            MethodInfo mi    = dType.GetMethod("Invoke") ?? throw new NullReferenceException("get method Invoke");
+            MethodInfo mi = dType.GetMethod("Invoke") ?? throw new NullReferenceException("get method Invoke");
             if (mi.ReturnType == typeof(void)) { throw new NotSupportedException("invalid return type (void)"); }
 
             Type[]                pTypes     = Array.ConvertAll(mi.GetParameters(), p => p.ParameterType);
@@ -66,32 +69,53 @@ namespace Exomia.Framework
                 parameters[i] = Expression.Parameter(pTypes[i]);
             }
 
-            return (TDelegate)Convert.ChangeType(
-                Expression.Lambda(
-                              dType,
-                              Expression.New(
-                                  mi.ReturnType.GetConstructor(pTypes)
-                               ?? throw new NullReferenceException(
+            return
+                Expression
+                    .Lambda(
+                        dType,
+                        Expression.New(
+                            mi.ReturnType.GetConstructor(pTypes)
+                         ?? throw new NullReferenceException(
 
-                                      // ReSharper disable once CoVariantArrayConversion (can be disabled cause we do no write operations at run-time)
-                                      $"can not create a constructor for {typeof(TDelegate)}"), parameters),
-                              parameters)
-                          .Compile(), dType);
+                                // ReSharper disable once CoVariantArrayConversion (can be disabled cause we do no write operations at run-time)
+                                $"can not create a constructor for {dType}"), parameters),
+                        parameters)
+                    .Compile();
         }
 
         /// <summary>
-        ///     Get a compiled lambda expression that can then be used
-        ///     to repeatedly create instances of that type using the constructor that
-        ///     the compiled lambda expression was created from.
+        ///     Get a compiled lambda expression that can then be used to repeatedly create instances of
+        ///     that type using the constructor that the compiled lambda expression was created from.
+        /// </summary>
+        /// <typeparam name="TDelegate">
+        ///     The type of object that the compiled lambda expression will
+        ///     create.
+        /// </typeparam>
+        /// <returns>
+        ///     A compiled lambda expression in the form of a delegate.
+        /// </returns>
+        public static TDelegate GetCreator<TDelegate>()
+            where TDelegate : Delegate
+        {
+            Type dType = typeof(TDelegate);
+            return (TDelegate)Convert.ChangeType(GetCreator(dType), dType);
+        }
+
+        /// <summary>
+        ///     Get a compiled lambda expression that can then be used to repeatedly create instances of
+        ///     that type using the constructor that the compiled lambda expression was created from.
         ///     Uses the constructor that matches the constructor parameters passed in.
         /// </summary>
-        /// <typeparam name="TRes">The type of object that the compiled lambda expression will create.</typeparam>
-        /// <param name="constructorParameters">An ordered array of the parameters the constructor takes.</param>
-        /// <returns>A compiled lambda expression in the form of a delegate.</returns>
+        /// <typeparam name="TRes"> The type of object that the compiled lambda expression will create. </typeparam>
+        /// <param name="constructorParameters"> An ordered array of the parameters the constructor takes. </param>
+        /// <returns>
+        ///     A compiled lambda expression in the form of a delegate.
+        /// </returns>
         /// <exception cref="ArgumentException">
-        ///     Thrown when T does not have a constructor that takes the passed in set of parameters.
+        ///     Thrown when T does not have a constructor that takes the
+        ///     passed in set of parameters.
         /// </exception>
-        public static Creator<TRes> GetCreator<TRes>(params Type[] constructorParameters) where TRes : class
+        public static Creator<TRes> GetCreator<TRes>(params Type[] constructorParameters)
         {
             if (constructorParameters == null) { constructorParameters = Type.EmptyTypes; }
 
@@ -114,6 +138,21 @@ namespace Exomia.Framework
                                                         "This type does not have a constructor that takes the passed in set of parameters.",
                                                         nameof(constructorParameters)), argsExpressions), param)
                                             .Compile();
+        }
+
+        /// <summary>
+        ///     Get a compiled lambda expression that can then be used to repeatedly create instances of
+        ///     that type using the constructor that the compiled lambda expression was created from.
+        ///     Uses the constructor that matches the constructor parameters passed in.
+        /// </summary>
+        /// <typeparam name="TRes"> The type of object that the compiled lambda expression will create. </typeparam>
+        /// <param name="constructorParameters"> An ordered array of the parameters the constructor takes. </param>
+        /// <returns>
+        ///     A compiled lambda expression in the form of a delegate.
+        /// </returns>
+        public static Creator<TRes> GetCreator<TRes>(IEnumerable<Type> constructorParameters)
+        {
+            return GetCreator<TRes>(constructorParameters?.ToArray());
         }
     }
 }
