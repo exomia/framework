808f19836be3367dbfe0d76055fae8b0bdeececd
Daniel Baetz <baetz.daniel@gmail.com>
2020-05-22 23:56:10 +0200

    ref

--- src/Exomia.Framework/Win32/Shell32.cs
+++ src/Exomia.Framework/Win32/Shell32.cs
@@ -4,8 +4,17 @@ using System.Security;
 
 namespace Exomia.Framework.Win32
 {
-    public static class Shell32
+    static class Shell32
     {
+        /// <summary>
+        ///     Extracts the icon.
+        /// </summary>
+        /// <param name="hInst">       The instance. </param>
+        /// <param name="exeFileName"> Filename of the executable file. </param>
+        /// <param name="iconIndex">   Zero-based index of the icon. </param>
+        /// <returns>
+        ///     The extracted icon.
+        /// </returns>
         [SuppressUnmanagedCodeSecurity]
         [DllImport("shell32.dll", CharSet = CharSet.Auto, BestFitMapping = false)]
         internal static extern IntPtr ExtractIcon(IntPtr hInst, string exeFileName, int iconIndex);
