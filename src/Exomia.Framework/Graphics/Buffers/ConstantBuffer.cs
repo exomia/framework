e2b190873cee401218bad1c785c072f83d95c448
Daniel Baetz <baetz.daniel@gmail.com>
2020-11-09 21:25:50 +0100

    ref

--- src/Exomia.Framework/Graphics/Buffers/ConstantBuffer.cs
+++ src/Exomia.Framework/Graphics/Buffers/ConstantBuffer.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Runtime.CompilerServices;
+using Exomia.Framework.Mathematics;
 using SharpDX.Direct3D11;
 using Buffer = SharpDX.Direct3D11.Buffer;
 
@@ -55,10 +56,37 @@ namespace Exomia.Framework.Graphics.Buffers
                                                       CpuAccessFlags  cpuAccessFlags = CpuAccessFlags.None)
             where T : unmanaged
         {
+            return Create(graphicsDevice, Math2.Ceiling(sizeof(T) / 16.0f) * 16, resourceUsage, cpuAccessFlags);
+        }
+
+        /// <summary>
+        ///     Creates a new <see cref="ConstantBuffer" /> with the specified size in bytes.
+        /// </summary>
+        /// <param name="graphicsDevice"> The graphics device. </param>
+        /// <param name="sizeInBytes">    The size in bytes. </param>
+        /// <param name="resourceUsage">  (Optional) The resource usage. </param>
+        /// <param name="cpuAccessFlags"> (Optional) The CPU access flags. </param>
+        /// <returns>
+        ///     A <see cref="ConstantBuffer" />.
+        /// </returns>
+        /// <remarks>
+        ///     The <paramref name="sizeInBytes" /> must be a multiple of 16,
+        ///     if not it will be calculated to the minimum multiple of 16 fitting it in.
+        ///     e.g.
+        ///     - a size in bytes of 11 will be 16.
+        ///     - a size in bytes of 23 will be 32.
+        ///     - a size in bytes of 80 will be 80.
+        ///     > see https://docs.microsoft.com/de-de/windows/win32/api/d3d11/ns-d3d11-d3d11_buffer_desc#remarks
+        /// </remarks>
+        public static ConstantBuffer Create(IGraphicsDevice graphicsDevice,
+                                            int             sizeInBytes,
+                                            ResourceUsage   resourceUsage  = ResourceUsage.Default,
+                                            CpuAccessFlags  cpuAccessFlags = CpuAccessFlags.None)
+        {
             return new ConstantBuffer(
                 new Buffer(
                     graphicsDevice.Device,
-                    sizeof(T),
+                    Math2.Ceiling(sizeInBytes / 16.0f) * 16,
                     resourceUsage,
                     BindFlags.ConstantBuffer,
                     cpuAccessFlags,
