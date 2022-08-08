ce9e5b5018da4cec901e4531648b95b891e2729d
Daniel Bätz <baetz.daniel@gmail.com>
2022-08-08 10:44:10 +0200

    typo

--- src/Exomia.Framework.Core/Resources/shaders/compile_shaders.bat
+++ src/Exomia.Framework.Core/Resources/shaders/compile_shaders.bat
@@ -7,5 +7,5 @@ for /r %%a in (src/*) do (
 	call %VULKAN_SDK%/bin/spirv-opt.exe -O %%~nxa.spv -o %%~nxa-opt.spv
 )
 @echo:
-@echo ------------- compiling and optimizing shaders finsihed! -------------
+@echo ------------- compiling and optimizing shaders finished! -------------
 @echo ----------------------------------------------------------------------
