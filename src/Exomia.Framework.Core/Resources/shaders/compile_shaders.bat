0afc36a71d99ecb01664b72783e3191b9d5c2f6e
Daniel Bätz <baetz.daniel@gmail.com>
2022-09-08 23:59:47 +0200

    updated shader compilation using version spirv1.6

--- src/Exomia.Framework.Core/Resources/shaders/compile_shaders.bat
+++ src/Exomia.Framework.Core/Resources/shaders/compile_shaders.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 @echo ----------------------------------------------------------------------
 @echo ------------- compiling and optimizing shaders started! --------------
 for /r %%a in (src/*) do (
-	call %VULKAN_SDK%/bin/glslangValidator.exe -V src/%%~nxa -o %%~nxa.spv
+	call %VULKAN_SDK%/bin/glslangValidator.exe --target-env spirv1.6 -V src/%%~nxa -o %%~nxa.spv
 	call %VULKAN_SDK%/bin/spirv-opt.exe -O %%~nxa.spv -o %%~nxa-opt.spv
 )
 @echo:
