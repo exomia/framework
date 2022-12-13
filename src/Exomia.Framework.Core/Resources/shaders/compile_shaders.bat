10bd0dc82f967c1e9f7984f66f9ebd95beb18c62
Daniel Bätz <baetz.daniel@gmail.com>
2022-12-13 18:09:49 +0100

    adjusted shader compilation for multiple frameworks

--- src/Exomia.Framework.Core/Resources/shaders/compile_shaders.bat
+++ src/Exomia.Framework.Core/Resources/shaders/compile_shaders.bat
@@ -1,15 +1,17 @@
 @echo off
 
 cd /d %~dp0
-if exist compiled rmdir /s/q compiled
-mkdir compiled
 
-@echo ----------------------------------------------------------------------
-@echo ------------- compiling and optimizing shaders started! --------------
+if exist bin\%1\compiled rmdir /s/q bin\%1\compiled
+
+mkdir bin\%1\compiled
+
+@echo -----------------------------------------------------------------------------
+@echo ------------- compiling and optimizing shaders for %1 started! --------------
 for /r %%a in (src/*.frag,src/*.vert) do (
-	call %VULKAN_SDK%/bin/glslangValidator.exe --target-env spirv1.6 -V src/%%~nxa -o compiled/%%~nxa.spv
-	call %VULKAN_SDK%/bin/spirv-opt.exe -O compiled/%%~nxa.spv -o compiled/%%~nxa-opt.spv
+	call %VULKAN_SDK%/bin/glslangValidator.exe --target-env spirv1.6 -V src/%%~nxa -o bin/%1/compiled/%%~nxa.spv
+	call %VULKAN_SDK%/bin/spirv-opt.exe -O bin/%1/compiled/%%~nxa.spv -o bin/%1/compiled/%%~nxa-opt.spv
 )
 @echo:
-@echo ------------- compiling and optimizing shaders finished! -------------
-@echo ----------------------------------------------------------------------
+@echo ------------- compiling and optimizing shaders for %1 finished! -------------
+@echo -----------------------------------------------------------------------------
